0b5508da275cd8580254f2f157b85dcc083f045d
Dennis Jakobi <dennis.jakobi76@gmail.com>
2026-01-05 14:37:40 +0100

    implement producer-consumer threads in MainTest

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -2,5 +2,38 @@ public class MainTest {
 
     public static void main(String[] args) {
 
+        SharedBuffer sharedBuffer = new SharedBuffer();
+
+        // producer thread
+        Thread producerThread = new Thread(() -> {
+
+            for (int i = 0; i < 5; i++){
+
+                try {
+                    sharedBuffer.produce(i);
+                    Thread.sleep(500L);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+        }, "Producer Thread");
+
+        // consumer thread
+        Thread consumerThread = new Thread(() -> {
+            for (int i = 0; i < 5; i++){
+
+                try {
+                    sharedBuffer.consume();
+                    Thread.sleep(800L);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+
+        }, "Consumer Thread");
+
+        producerThread.start();
+        consumerThread.start();
     }
 }
